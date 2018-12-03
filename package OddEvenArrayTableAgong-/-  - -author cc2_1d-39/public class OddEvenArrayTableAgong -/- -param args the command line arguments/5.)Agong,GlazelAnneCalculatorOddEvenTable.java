05867a3ae526f2a64baaae2feabf285ed7413117
AgongGlazel <45381553+AgongGlazel@users.noreply.github.com>
2018-12-03 20:49:31 +0800

    Create 5.)Agong,GlazelAnneCalculatorOddEvenTable.java

---
+++ package OddEvenArrayTableAgong-/-  - -author cc2_1d-39/public class OddEvenArrayTableAgong -/- -param args the command line arguments/5.)Agong,GlazelAnneCalculatorOddEvenTable.java
@@ -0,0 +1,43 @@
+package OddEvenArrayTableAgong;
+/**
+ *
+ * @author cc2_1d-39
+ */
+public class OddEvenArrayTableAgong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     
+     int nbr[] = {3 ,9 ,15 ,20 ,65 ,23 ,18 ,4 ,2 ,14 };
+     
+    
+     
+     int[] odd = new int[nbr.length];
+     int[] even = new int[nbr.length];
+     
+     int oddn = 0;
+     int evenn = 0;
+     
+     for (int n = 0; n< nbr.length; n++){
+     
+     if (nbr[n]% 2==0){
+         even [evenn]=nbr[n];
+         evenn++;
+                 
+         
+     }else{
+         odd[oddn] = nbr[n]; 
+         oddn++;
+         
+     }
+     }
+    
+      System.out.println("Odd \t Even");
+         for (int n = 0; n < oddn; n++){
+        System.out.println(odd[n] +"\t"+ even[n]);
+         }
+    }
+}
